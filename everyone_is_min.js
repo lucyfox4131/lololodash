3c9b50dfddbda460c5d9642b02c48a45c0d770d9
Lucy Fox <lucyreneefox@gmail.com>
2016-08-08 13:33:23 -0600

    Add everyone is min

--- everyone_is_min.js
+++ everyone_is_min.js
@@ -1,15 +1,24 @@
 var _ = require('lodash');
 
 var worker = function(collection){
-  var temps = {hot: [], warm: []};
-  return _.forEach(collection, function(item){
-      if (_.every(item.values, < 19)){
-        temps.hot << item
-      } else if (_.some(item.values, 19)){
-        temps.warm << item
+  var temps = {
+                hot: [],
+                warm: []
+              };
+
+  function isHot(collection){
+    return collection > 19;
+  };
+
+  _.forEach(collection, function(town, name){
+      if (_.every(town, isHot)){
+        temps.hot.push(name);
+      } else if (_.some(town, isHot)){
+        temps.warm.push(name);
       }
-    }
-  )
+    });
+
+  return temps;
 };
 
 module.exports = worker;
